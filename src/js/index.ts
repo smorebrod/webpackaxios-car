0bb50a19e652f69d0a5fb3314602c214f0a42e4c
smorebrod <36259392+smorebrod@users.noreply.github.com>
2019-03-27 11:19:00 +0100

    tilføjet interface + get metode

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,23 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError,
+} from "../../node_modules/axios/index"
+import { ICar } from "./Icar";
+
+let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+
+function showAllCArs():void{
+axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
+.then(function (response : AxiosResponse<ICar[]>): void
+{
+
+})
+.catch(
+    function (error : AxiosError) : void{
+        console.log(error);
+    }
+)
+
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+showAllCArs()
